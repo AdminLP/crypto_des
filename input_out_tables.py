830417aebae0ad5110255ef9f9506519b8883fec
sound <soundbylp@gmail.com>
2016-10-13 03:25:35 +0300

    output1 and output2 calculate

--- input_out_tables.py
+++ input_out_tables.py
@@ -2,6 +2,7 @@ from operator import xor
 
 input1 = [x for x in range(16)]
 deltaA = [x for x in range(16)]
+S1 = [[1, 6, 2, 1, 5, 0, 7, 2], [4, 5, 6, 3, 4, 7, 3, 0]]
 
 
 def int2bin(n, count=4):
@@ -14,11 +15,19 @@ def calculate_input2(input1, deltaA):
 input2 = calculate_input2(input1, deltaA)
 
 
+def output_calculation_after_table_s1(input, table):
+    return [table[int(int2bin(input[i])[0])][int(int2bin(input[i])[1:], 2)] for i in range(16)]
+
+
+output1 = output_calculation_after_table_s1(input1, S1)
+output2 = [output_calculation_after_table_s1(input2[i], S1) for i in range(16)]
+
+
 def beautiful_output_in_the_console(input1, input2):
     for i in range(16):
-        print('Вход_1' + '\t\t' + 'Вход_2' + '\t\t' + 'Выход_1' + '\t\t' + 'Выход_2' + '\t\t' + 'дельта_C')
+        print('Вход_1' + '\t\t' + 'Вход_2' + '\t\t' + 'Выход_1' + '\t\t' + 'Выход_2' + '\t\t' + 'Дельта_C')
         for j in range(16):
-            print(int2bin(input1[j]) + '\t\t' + int2bin(input2[i][j]))
+            print(int2bin(input1[j]) + '\t\t' + int2bin(input2[i][j]) + '\t\t' + int2bin(output1[j]) + '\t\t' + int2bin(output2[i][j]))
         print('\n')
 
 beautiful_output_in_the_console(input1, input2)
