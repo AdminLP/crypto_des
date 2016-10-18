7ad480c9a611bd0b3043a2589ede91a17fc424af
sound <soundbylp@gmail.com>
2016-10-18 13:00:40 +0300

    add S2 and S3 tables

--- input_out_tables.py
+++ input_out_tables.py
@@ -3,6 +3,8 @@ from operator import xor
 input1 = [x for x in range(16)]
 deltaA = [x for x in range(16)]
 S1 = [[1, 6, 2, 1, 5, 0, 7, 2], [4, 5, 6, 3, 4, 7, 3, 0]]
+S2 = [[1, 3, 0, 2, 3, 5, 6, 2], [7, 4, 5, 1, 4, 6, 0, 7]]
+S3 = [[2, 2, 0, 0], [1, 0, 0, 2], [2, 1, 3, 3], [1, 1, 3, 3]]
 
 
 def int2bin(n, count=4):
@@ -12,24 +14,34 @@ def int2bin(n, count=4):
 def calculate_input2(input1, deltaA):
     return [[xor(input1[j], deltaA[i]) for j in input1] for i in deltaA]
 
-input2 = calculate_input2(input1, deltaA)
 
-
-def output_calculation_after_table_s1(input, table):
+def output_calculation_after_table_2x8(input, table):
     return [table[int(int2bin(input[i])[0])][int(int2bin(input[i])[1:], 2)] for i in range(16)]
+
+
+def output_calculation_after_table_4x4(input, table):
+    return [table[int((int2bin(input1[i])[0] + int2bin(input1[i])[-1]), 2)][int(int2bin(input[i])[1:-1], 2)] for i in range(16)]
 
 
 def calculate_delta_c(output1, output2):
     return [[xor(output1[j], output2[i][j]) for j in range(16)] for i in range(16)]
 
-output1 = output_calculation_after_table_s1(input1, S1)
-output2 = [output_calculation_after_table_s1(input2[i], S1) for i in range(16)]
-deltaC = calculate_delta_c(output1, output2)
-# print(output1)
-# print(output2)
+
+input2 = calculate_input2(input1, deltaA)
+output1_S1 = output_calculation_after_table_2x8(input1, S1)
+output2_S1 = [output_calculation_after_table_2x8(input2[i], S1) for i in range(16)]
+deltaC_S1 = calculate_delta_c(output1_S1, output2_S1)
+
+output1_S2 = output_calculation_after_table_2x8(input1, S2)
+output2_S2 = [output_calculation_after_table_2x8(input2[i], S2) for i in range(16)]
+deltaC_S2 = calculate_delta_c(output1_S2, output2_S2)
+
+output1_S3 = output_calculation_after_table_4x4(input1, S3)
+output2_S3 = [output_calculation_after_table_4x4(input2[i], S3) for i in range(16)]
+deltaC_S3 = calculate_delta_c(output1_S3, output2_S3)
 
 
-def beautiful_output_in_the_console(input1, input2):
+def beautiful_output_in_the_console(input1, input2, output1, output2, deltaC):
     for i in range(16):
         print('Таблица №' + str(i+1))
         print('-' * 74)
@@ -46,5 +58,19 @@ def beautiful_output_in_the_console(input1, input2):
         print('-' * 74)
         print('\n')
 
-beautiful_output_in_the_console(input1, input2)
-
+print('S1 Table')
+beautiful_output_in_the_console(input1, input2, output1_S1, output2_S1, deltaC_S1)
+print('S2 Table')
+beautiful_output_in_the_console(input1, input2, output1_S2, output2_S2, deltaC_S2)
+print('S3 Table')
+beautiful_output_in_the_console(input1, input2, output1_S3, output2_S3, deltaC_S3)
+# print(deltaC[1])
+# the_number_of_occurrences = [{} for i in range(16)]
+# print(the_number_of_occurrences)
+# for i in range(16):
+#     for n in deltaC[i]:
+#         if n in the_number_of_occurrences[i].keys():
+#             the_number_of_occurrences[i][n] += 1
+#         else:
+#             the_number_of_occurrences[i][n] = 1
+#     print(the_number_of_occurrences[i])
